02b2536c494d17557cfc74ad1b3f5bee94403b33
thisismactan <mactan@gmail.com>
2023-11-06 21:15:10 -0500

    updated presidential results and state priors with 2020 results

--- src/state_partisan_priors.R
+++ src/state_partisan_priors.R
@@ -1,8 +1,8 @@
 source("src/library.R")
 
-regions <- read_csv("data/state_divisions.csv")
+regions <- read_csv("data/state_divisions.csv", lazy = FALSE)
 
-historical_results <- read_csv("data/presidential_election_results_by_state.csv") %>%
+historical_results <- read_csv("data/presidential_election_results_by_state.csv", lazy = FALSE) %>%
   group_by(year, state) %>%
   mutate(two_party_pct = votes / sum(votes)) %>%
   ungroup() %>%
@@ -10,7 +10,8 @@ historical_results <- read_csv("data/presidential_election_results_by_state.csv"
   mutate(national_winner = case_when(year %in% 1992:1996 ~ "Bill Clinton",
                                      year %in% 2000:2004 ~ "George W. Bush",
                                      year %in% 2008:2012 ~ "Barack Obama",
-                                     year == 2016 ~ "Donald Trump"))
+                                     year == 2016 ~ "Donald Trump",
+                                     year == 2020 ~ "Joe Biden"))
 
 national_historical_results <- historical_results %>%
   filter(!grepl("congressional", state)) %>%
@@ -23,12 +24,12 @@ national_historical_results <- historical_results %>%
   dplyr::select(year, two_party_margin_natl) %>%
   ungroup()
 
-two_party_margin_2016 <- national_historical_results %>%
-  filter(year == 2016) %>%
+two_party_margin_2020 <- national_historical_results %>%
+  filter(year == 2020) %>%
   pull(two_party_margin_natl)
 
-state_two_party_margins_2016 <- historical_results %>%
-  filter(year == 2016) %>%
+state_two_party_margins_2020 <- historical_results %>%
+  filter(year == 2020) %>%
   dplyr::select(-year, -votes, -national_winner) %>%
   spread(party, two_party_pct) %>%
   mutate(state_two_party_margin = Democratic - Republican) %>%
@@ -48,13 +49,13 @@ incumbent_running_results <- historical_results %>%
   left_join(regions, by = "state")
 
 # Linear regression
-two_party_margin_change_model <- lm(two_party_margin ~ last_two_party_margin + two_party_margin_natl_change, data = incumbent_running_results)
+two_party_margin_change_model <- lm(I(two_party_margin - last_two_party_margin) ~ two_party_margin_natl_change, data = incumbent_running_results)
 summary(two_party_margin_change_model)
 
 # With regions
-two_party_margin_change_model_regions <- lmer(two_party_margin ~ last_two_party_margin + two_party_margin_natl_change + (1|region), 
+two_party_margin_change_model_regions <- lmer(I(two_party_margin - last_two_party_margin) ~ two_party_margin_natl_change + (1|region), 
                                               data = incumbent_running_results)
-fixed_effect_coefficients <- as.matrix(coefficients(two_party_margin_change_model_regions)$region)[1, 2:3]
+fixed_effect_coefficients <- as.matrix(coefficients(two_party_margin_change_model_regions)$region)[1, 2]
 
 # Variance components
 regional_sd <- sqrt(as.numeric(VarCorr(two_party_margin_change_model_regions)))
